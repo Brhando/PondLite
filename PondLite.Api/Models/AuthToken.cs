7ae97707226eec52b14d2521f9d9d9147834a69c
Brhando <brandonsisco21@gmail.com>
2026-05-04 14:35:13 -0700

    add UserAccount and AuthToken models

---
+++ PondLite.Api/Models/AuthToken.cs
@@ -0,0 +1,17 @@
+﻿namespace PondLite.Api.Models
+{
+    public class AuthToken
+    {
+        public Guid TokenId { get; set; } = Guid.NewGuid();
+
+        public Guid UserAccountId { get; set; }
+
+        public string TokenValue { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime ExpiresAt {  get; set; } = DateTime.UtcNow.AddMinutes(30);
+
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ PondLite.Api/Models/UserAccount.cs
@@ -0,0 +1,16 @@
+﻿namespace PondLite.Api.Models
+{
+    public class UserAccount
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Username { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
