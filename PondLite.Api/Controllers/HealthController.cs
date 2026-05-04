c703e0c9704186cddf3414499c3cda669cec4473
Brhando <brandonsisco21@gmail.com>
2026-05-04 14:07:40 -0700

    add HealthController.cs to allow for a wuick backend running check

---
+++ PondLite.Api/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PondLite.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok(new
+            {
+                status = "PondLite API is running",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
